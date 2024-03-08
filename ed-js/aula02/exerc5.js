a7949c13acf6a23567674e6c58b9e383f3f65cf8
daviatrock@gmail.com <daviatrock@gmail.com>
2024-03-07 21:56:58 -0300

    exerciciosjs2

---
+++ ed-js/aula02/exerc5.js
@@ -0,0 +1,15 @@
+var month = 5;
+switch (month) {
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('January');
+        break;
+    case 3:
+        console.log('January');
+        break;
+    default:
+        console.log('Month is not January, February or March');
+        break;
+}
